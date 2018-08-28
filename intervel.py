356b79f4000bbfb678e16b2a814387978dd2cd17
jyothi1998 <42638405+jyothi1998@users.noreply.github.com>
2018-08-28 22:25:43 +0530

    Update intervel.py

--- intervel.py
+++ intervel.py
@@ -1,8 +1,8 @@
-lower=int(raw_input())
-upper=int(raw_input())
-for x in range(lower,upper+1):
-    if (x%2!=0):
-        if x!=1:
-            print(x)
+lower1,upper1=map(int,raw_input().split())
+for i in range(lower1+1,upper1+1):
+  if(i%2!=0):
+    print(i),
+
+
             
 
