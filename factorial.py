26fe90f11e9a21c91e0edc2146799bef5f913af1
jyothi1998 <42638405+jyothi1998@users.noreply.github.com>
2018-08-31 06:40:50 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,8 @@
-n = int(raw_input())
-fact = 1
-if n < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif n == 0:
-     print (n)
+n=int(raw_input())
+fact=1
+if n==0:
+   print(1)
 else:
-   for i in range(1,n + 1):
-       fact = fact*i
+   for i in range(1,n+1):
+      fact=fact*i
    print(fact)
