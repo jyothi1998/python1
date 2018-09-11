6ae3555a6dc001c62749786e92a0f009933a1f53
jyothi1998 <42638405+jyothi1998@users.noreply.github.com>
2018-09-11 14:51:08 +0530

    Create 71.py

---
+++ 71.py
@@ -0,0 +1,22 @@
+# function which return reverse of a string 
+def reverse(n): 
+    return n[::-1] 
+  
+def isPalindrome(n): 
+    # Calling reverse function 
+    rev = reverse(n) 
+  
+    # Checking if both string are equal or not 
+    if (n == rev): 
+        return True
+    return False
+  
+  
+# Driver code 
+n = "malayalam"
+ans = isPalindrome(n) 
+  
+if ans == 1: 
+    print("yes") 
+else: 
+    print("no")
