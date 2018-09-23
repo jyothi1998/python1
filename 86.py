f68b7c33a43f27909d7a35ff3488ad33f6542bdf
jyothi1998 <42638405+jyothi1998@users.noreply.github.com>
2018-09-23 05:36:15 +0100

    Create 86.py

---
+++ 86.py
@@ -0,0 +1,10 @@
+def isIsogram(s):
+	charMap = {}
+	for c in s:
+		if c in charMap:
+			return False
+		else:
+			charMap[c] = 1
+	return True
+s = raw_input().rstrip()
+print("Yes" if isIsogram(s) else "No")
